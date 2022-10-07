28f052a89025b2bbcf5fd1fb0c35abed2cb67436
benjaminrall <benr2508@aol.com>
2022-10-07 12:20:36 +0100

    edits

--- AssemblyCode/AssemblyException.cs
+++ AssemblyCode/AssemblyException.cs
@@ -4,9 +4,6 @@ namespace AssemblyCode
 {
     public class AssemblyException : Exception
     {
-        public AssemblyException(string message) : base(message)
-        {
-            
-        }
+        public AssemblyException(string message) : base(message) { }
     }
 }
